8316c7167f1f22938eff5b66a26a47f517da532c
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-09-21 12:28:06 -0700

    Routes Complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Forms from './Forms'
-
+import Forms from "./Forms";
 
 function App() {
   return (
--- src/Forms.js
+++ src/Forms.js
@@ -1,25 +1,24 @@
-import React from 'react';
+import React from "react";
+import "./Forms.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-
-import Login from './Login'
-import Signup from './Signup'
-
+import Login from "./Login";
+import Signup from "./Signup";
 
 export default function Forms() {
-    return (
-        <div>
-            <p>Main Background</p>
-            <Router>
-                <Switch>
-                    <Route path="/login">
-                        <Login />
-                    </Route>
-                    <Route path="/signup">
-                        <Signup />
-                    </Route>
-                </Switch>
-            </Router>
-        </div>
-    )
+  return (
+    <div className="form-background">
+      <p>Main Background</p>
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
